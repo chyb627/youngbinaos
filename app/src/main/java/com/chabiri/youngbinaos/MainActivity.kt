4c4a6072c99c721e6ee42014496b62675816142c
cha <chyb627@naver.com>
2023-04-30 21:23:15 +0900

    숫자세기 앱

--- app/src/main/java/com/chabiri/youngbinaos/MainActivity.kt
+++ app/src/main/java/com/chabiri/youngbinaos/MainActivity.kt
@@ -2,10 +2,31 @@ package com.chabiri.youngbinaos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val numberTextView = findViewById<TextView>(R.id.numberTextView)
+        val resetButton = findViewById<Button>(R.id.resetButton)
+        val plusButton = findViewById<Button>(R.id.plusButton)
+
+        var number = 0
+
+        resetButton.setOnClickListener {
+            number = 0
+            numberTextView.text = number.toString()
+            Log.d("onClickReset::","리셋 된 숫자는 $number.")
+        }
+
+        plusButton.setOnClickListener {
+            number += 1
+            numberTextView.text = number.toString()
+            Log.d("onClickPlus::","플러스 된 숫자는 $number.")
+        }
     }
 }
